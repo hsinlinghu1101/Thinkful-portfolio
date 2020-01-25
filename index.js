037b2284a4dcc469785042c7fd072c27ecb33f5c
hsinlinghu1101 <47201201+hsinlinghu1101@users.noreply.github.com>
2020-01-25 12:11:50 -0800

    updat html, css and js files

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 
-$('.startButton').on('click', function(event){
-  event.preventDefault();
-  $('.main').removeAttr('hidden');
-  $('main')[0].scrollIntoView();
-});
-
 $('.icon').click(function(event){
   event.preventDefault();
   $('.spr').toggle();
